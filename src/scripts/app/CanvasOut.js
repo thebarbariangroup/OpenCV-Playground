562d2873fa16274420c54990037520f91f236d53
CCliff <clcliff30@gmail.com>
2019-05-02 11:41:29 -0400

    Tests

--- src/scripts/app/CanvasOut.js
+++ src/scripts/app/CanvasOut.js
@@ -17,10 +17,10 @@ export default class CanvasOut {
   }
 
   streamInputToOutput (time) {
+    this._transformInput(time);
     this.context.drawImage(this.input.getOutput(), 0, 0, this.state.width, this.state.height);
     this.srcMat.data.set(this.context.getImageData(0, 0, this.state.width, this.state.height).data);
-    this._transformInput(time);
-    cv.imshow(this.output, this.dstMat)
+
 
     requestAnimationFrame((time) => {
       this.streamInputToOutput(time);
@@ -59,14 +59,23 @@ export default class CanvasOut {
     this.mask = this.mask || new cv.Mat();
 
     const currentTime = time - this.prevTime;
-    // this.prevSrcMat = this.prevSrcMat || new cv.Mat();
-    if (this.prevSrcMat) {
-      cv.subtract(this.srcMat, this.prevSrcMat, this.dstMat, this.mask, -1);
-    }
 
-    if (currentTime > 16) {
+    if (currentTime > 3000) {
+      // this.mask = this.mask || cv.Mat.ones(640, 480, cv.CV_8UC4);
+
+      if (this.prevSrcMat) {
+        console.log(this.prevSrcMat.data[1], this.srcMat.data[1]);
+        cv.subtract(this.srcMat, this.prevSrcMat, this.dstMat);
+        console.log(this.dstMat.data[1]);
+      }
+
       this.prevSrcMat = this.srcMat;
       this.prevTime = time;
+
+      cv.imshow(this.output, this.dstMat)
+      // this.prevSrcMat.delete();
+      // this.srcMat.delete();
+      // this.dstMat.delete();
     }
 
   }
